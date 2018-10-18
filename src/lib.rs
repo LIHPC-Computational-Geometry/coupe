f72215d6b439779a9212edb8b45724b3bd9c108f
Armand Touminet <armand.touminet@protonmail.com>
2018-10-18 14:56:10 +0200

    Add region refinement utilities methods to Aabb and Mbr structures

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@
 extern crate approx;
 #[macro_use]
 extern crate itertools;
+#[macro_use]
+extern crate lazy_static;
 extern crate nalgebra;
+extern crate radix;
 extern crate snowflake;
 
 pub mod algorithms;
