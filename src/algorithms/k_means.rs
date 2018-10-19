6ddaaca27993e87751c2a98154148ead2298b25a
Armand Touminet <armand.touminet@protonmail.com>
2018-10-19 14:20:04 +0200

    Finished implementing kmeans

--- src/algorithms/k_means.rs
+++ src/algorithms/k_means.rs
@@ -7,46 +7,151 @@ use itertools::Itertools;
 use snowflake::ProcessUniqueId;
 
 use std::cmp::Ordering;
-use std::collections::HashMap;
+
+use super::z_curve;
 
 /// A wrapper type for ProcessUniqueId
 /// to enforce that it represents temporary ids
 /// for the k-means algorithm and not a partition id
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct ClusterId(ProcessUniqueId);
+type ClusterId = ProcessUniqueId;
+// #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+// struct ClusterId(ProcessUniqueId);
 
-impl ClusterId {
-    pub fn new() -> Self {
-        ClusterId(ProcessUniqueId::new())
-    }
-}
+// impl ClusterId {
+//     pub fn new() -> Self {
+//         ClusterId(ProcessUniqueId::new())
+//     }
+// }
+
+const MAX_ITER: usize = 100;
 
 pub fn balanced_k_means(
     points: Vec<Point2D>,
     num_partitions: usize,
     epsilon: f64,
-    maximum_imbalance: f64,
-    deltha_threshold: f64,
+    delta_threshold: f64,
 ) -> Vec<(Point2D, ProcessUniqueId)> {
-    unimplemented!()
+    // custom weights are not yet supported
+    let weights: Vec<_> = points.iter().map(|_| 1.).collect();
+
+    // sort points with Z-curve
+    let qt = z_curve::ZCurveQuadtree::from_points(points);
+    let points = qt.reorder();
+
+    let points_per_center = points.len() / num_partitions;
+
+    // select num_partitions initial centers from the ordered points
+    let centers: Vec<_> = points.iter().cloned().step_by(points_per_center).collect();
+
+    let center_ids: Vec<_> = centers.iter().map(|_| ClusterId::new()).collect();
+    let assignments: Vec<_> = center_ids
+        .iter()
+        .cloned()
+        .flat_map(|id| ::std::iter::repeat(id).take(points_per_center))
+        .take(points.len())
+        .collect();
+
+    let influences: Vec<_> = centers.iter().map(|_| 1.).collect();
+    let lbs: Vec<_> = points.iter().map(|_| 0.).collect();
+    let ubs: Vec<_> = points.iter().map(|_| 1.).collect();
+
+    balanced_k_means_iter(
+        centers,
+        center_ids,
+        points,
+        weights,
+        influences,
+        assignments,
+        ubs,
+        lbs,
+        epsilon,
+        MAX_ITER,
+        delta_threshold,
+    )
+}
+
+fn balanced_k_means_iter(
+    centers: Vec<Point2D>,
+    center_ids: Vec<ClusterId>,
+    points: Vec<Point2D>,
+    weights: Vec<f64>,
+    influences: Vec<f64>,
+    assignments: Vec<ClusterId>,
+    ubs: Vec<f64>,
+    lbs: Vec<f64>,
+    epsilon: f64,
+    current_iter: usize,
+    delta_threshold: f64,
+) -> Vec<(Point2D, ClusterId)> {
+    // FIX: remove the clones
+    let (assignments, influences, mut ubs, mut lbs) = assign_and_balance(
+        centers.clone(),
+        &center_ids,
+        points.clone(),
+        &weights,
+        influences,
+        assignments,
+        ubs,
+        lbs,
+        0.3,
+        MAX_ITER,
+    );
+
+    let new_centers: Vec<_> = assignments
+        .iter()
+        .zip(centers.iter().cloned())
+        .into_group_map()
+        .into_iter()
+        .map(|(_, points)| geometry::center(&points))
+        .collect();
+
+    let distances_moved: Vec<_> = centers
+        .into_iter()
+        .zip(new_centers.clone())
+        .map(|(c1, c2)| (c1 - c2).norm())
+        .collect();
+
+    let delta_max = distances_moved
+        .iter()
+        .max_by(|d1, d2| d1.partial_cmp(d2).unwrap_or(Ordering::Equal))
+        .unwrap();
+
+    if *delta_max < delta_threshold || current_iter == 0 {
+        points.into_iter().zip(assignments).collect()
+    } else {
+        relax_bounds(&mut lbs, &mut ubs, &distances_moved, &influences);
+        balanced_k_means_iter(
+            new_centers,
+            center_ids,
+            points,
+            weights,
+            influences,
+            assignments,
+            ubs,
+            lbs,
+            epsilon,
+            current_iter - 1,
+            delta_threshold,
+        )
+    }
 }
 
 fn assign_and_balance(
-    mut centers: Vec<Point2D>,
-    center_ids: Vec<ClusterId>,
+    centers: Vec<Point2D>,
+    center_ids: &[ClusterId],
     mut local_points: Vec<Point2D>,
-    mut weights: Vec<f64>,
+    weights: &[f64],
     mut influences: Vec<f64>,
     mut assignments: Vec<ClusterId>,
     mut ubs: Vec<f64>,
     mut lbs: Vec<f64>,
-    mut epsilon: f64,
-    mut max_iter: usize,
+    epsilon: f64,
+    max_iter: usize,
 ) -> (
     Vec<ClusterId>, // assignments
     Vec<f64>,       // influences
-    Vec<f64>,       // ub
-    Vec<f64>,       // lb
+    Vec<f64>,       // ubs
+    Vec<f64>,       // lbs
 ) {
     let mbr = Mbr2D::from_points(local_points.iter());
     let distances_to_mbr = centers
@@ -54,8 +159,6 @@ fn assign_and_balance(
         .zip(influences.iter())
         .map(|(center, influence)| mbr.distance_to_point(center) / influence)
         .collect::<Vec<_>>();
-
-    let local_block_sizes: Vec<usize> = centers.iter().map(|_| 0).collect();
 
     let (centers, distances_to_mbr): (Vec<_>, Vec<_>) = centers
         .into_iter()
@@ -140,28 +243,32 @@ fn assign_and_balance(
             .map(|(center, (_, new_center))| (*center - new_center).norm())
             .collect();
 
-        let max_distance_influence_ratio = distances_to_old_centers
-            .iter()
-            .zip(influences.iter())
-            .map(|(distance, influence)| distance / influence)
-            .max_by(|r1, r2| r1.partial_cmp(r2).unwrap_or(Ordering::Equal))
-            .unwrap_or(0.);
-
-        // relax lower and upper bounds according to influence
-        // modification.
-        ubs.iter_mut()
-            .zip(distances_to_old_centers)
-            .zip(influences.iter())
-            .for_each(|((ub, distance), influence)| {
-                *ub -= distance / influence;
-            });
-
-        lbs.iter_mut().for_each(|lb| {
-            *lb += max_distance_influence_ratio;
-        });
+        relax_bounds(&mut lbs, &mut ubs, &distances_to_old_centers, &influences);
     }
 
     (assignments, influences, lbs, ubs)
+}
+
+// relax lower and upper bounds according to influence
+// modification.
+fn relax_bounds(lbs: &mut [f64], ubs: &mut [f64], distances_moved: &[f64], influences: &[f64]) {
+    let max_distance_influence_ratio = distances_moved
+        .iter()
+        .zip(influences.iter())
+        .map(|(distance, influence)| distance / influence)
+        .max_by(|r1, r2| r1.partial_cmp(r2).unwrap_or(Ordering::Equal))
+        .unwrap_or(0.);
+
+    ubs.iter_mut()
+        .zip(distances_moved)
+        .zip(influences.iter())
+        .for_each(|((ub, distance), influence)| {
+            *ub -= distance / influence;
+        });
+
+    lbs.iter_mut().for_each(|lb| {
+        *lb += max_distance_influence_ratio;
+    });
 }
 
 fn imbalance(weights: &[f64]) -> f64 {
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 #[cfg(test)]
 #[macro_use]
 extern crate approx;
-#[macro_use]
 extern crate itertools;
-#[macro_use]
 extern crate lazy_static;
 extern crate nalgebra;
 extern crate snowflake;
