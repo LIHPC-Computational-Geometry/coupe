c80d11c4729003bd1e18c6971b332ea5de5c7dd3
Hubert Hirtz <hubert@hirtz.pm>
2021-09-28 16:11:33 +0200

    Fix benchmarks
    - Include changes from the const-generics overhaul
    - Include changes from the dependency updates

--- benches/generator.rs
+++ benches/generator.rs
@@ -4,17 +4,17 @@ use rand::{self, Rng};
 
 pub fn uniform_f64(min: f64, max: f64, num_vals: usize) -> Vec<f64> {
     let mut rng = rand::thread_rng();
-    (0..num_vals).map(|_| rng.gen_range(min, max)).collect()
+    (0..num_vals).map(|_| rng.gen_range(min..max)).collect()
 }
 
 pub fn uniform_rectangle(p_min: Point2D, p_max: Point2D, num_points: usize) -> Vec<Point2D> {
     let mut rng = rand::thread_rng();
     (0..num_points)
         .map(|_| {
-            Point2D::new(
-                rng.gen_range(p_min.x, p_max.x),
-                rng.gen_range(p_min.y, p_max.y),
-            )
+            Point2D::from([
+                rng.gen_range(p_min.x..p_max.x),
+                rng.gen_range(p_min.y..p_max.y),
+            ])
         })
         .collect()
 }
@@ -27,14 +27,11 @@ pub fn already_x_sorted_rectangle(
     let mut rng = rand::thread_rng();
     (0..num_points)
         .map(|_| {
-            Point2D::new(
-                rng.gen_range(p_min.x, p_max.x),
-                rng.gen_range(p_min.y, p_max.y),
-            )
+            Point2D::from([
+                rng.gen_range(p_min.x..p_max.x),
+                rng.gen_range(p_min.y..p_max.y),
+            ])
         })
-        .sorted_by(|p1, p2| {
-            p1.x.partial_cmp(&p2.x)
-                .unwrap_or(::std::cmp::Ordering::Equal)
-        })
+        .sorted_by(|p1, p2| p1.x.partial_cmp(&p2.x).unwrap_or(std::cmp::Ordering::Equal))
         .collect()
 }
