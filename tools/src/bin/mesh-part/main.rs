b84d335ea00c6ae684723b3c231eb4b0c287498d
Hubert Hirtz <hubert@hirtz.pm>
2022-04-05 11:30:55 +0200

    mesh-part: rework trait and share it with the other binaries
    this rework allows for efficient algorithm re-runs in benchmarks.

--- tools/src/bin/mesh-part/main.rs
+++ tools/src/bin/mesh-part/main.rs
@@ -1,352 +1,40 @@
 use anyhow::Context as _;
 use anyhow::Result;
-use coupe::Partition as _;
-use coupe::PointND;
 use mesh_io::medit::Mesh;
 use mesh_io::weight;
-use rayon::iter::IntoParallelRefIterator as _;
-use rayon::iter::ParallelIterator as _;
-use std::any;
 use std::env;
 use std::fs;
 use std::io;
-use std::mem;
 use tracing_subscriber::filter::EnvFilter;
 use tracing_subscriber::layer::SubscriberExt as _;
 use tracing_subscriber::util::SubscriberInitExt as _;
 use tracing_subscriber::Registry;
 use tracing_tree::HierarchicalLayer;
 
-#[cfg(feature = "metis")]
-mod metis;
-#[cfg(feature = "scotch")]
-mod scotch;
-
-struct Problem<const D: usize> {
-    points: Vec<PointND<D>>,
-    weights: weight::Array,
-    adjacency: sprs::CsMat<f64>,
-}
-
-trait Algorithm<const D: usize> {
-    fn run(&mut self, partition: &mut [usize], problem: &Problem<D>) -> Result<()>;
-}
-
-impl<const D: usize, R> Algorithm<D> for coupe::Random<R>
-where
-    R: rand::Rng,
-{
-    fn run(&mut self, partition: &mut [usize], _: &Problem<D>) -> Result<()> {
-        self.partition(partition, ())?;
-        Ok(())
-    }
-}
-
-impl<const D: usize> Algorithm<D> for coupe::Greedy {
-    fn run(&mut self, partition: &mut [usize], problem: &Problem<D>) -> Result<()> {
-        use weight::Array::*;
-        match &problem.weights {
-            Integers(is) => {
-                let weights = is.iter().map(|weight| weight[0]);
-                self.partition(partition, weights)?;
-            }
-            Floats(fs) => {
-                let weights = fs.iter().map(|weight| coupe::Real::from(weight[0]));
-                self.partition(partition, weights)?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl<const D: usize> Algorithm<D> for coupe::KarmarkarKarp {
-    fn run(&mut self, partition: &mut [usize], problem: &Problem<D>) -> Result<()> {
-        use weight::Array::*;
-        match &problem.weights {
-            Integers(is) => {
-                let weights = is.iter().map(|weight| weight[0]);
-                self.partition(partition, weights)?;
-            }
-            Floats(fs) => {
-                let weights = fs.iter().map(|weight| coupe::Real::from(weight[0]));
-                self.partition(partition, weights)?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl<const D: usize> Algorithm<D> for coupe::CompleteKarmarkarKarp {
-    fn run(&mut self, partition: &mut [usize], problem: &Problem<D>) -> Result<()> {
-        use weight::Array::*;
-        match &problem.weights {
-            Integers(is) => {
-                let weights = is.iter().map(|weight| weight[0]);
-                self.partition(partition, weights)?;
-            }
-            Floats(fs) => {
-                let weights = fs.iter().map(|weight| coupe::Real::from(weight[0]));
-                self.partition(partition, weights)?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl<const D: usize> Algorithm<D> for coupe::VnBest {
-    fn run(&mut self, partition: &mut [usize], problem: &Problem<D>) -> Result<()> {
-        use weight::Array::*;
-        match &problem.weights {
-            Integers(is) => {
-                let weights = is.iter().map(|weight| weight[0]);
-                self.partition(partition, weights)?;
-            }
-            Floats(fs) => {
-                let weights = fs.iter().map(|weight| coupe::Real::from(weight[0]));
-                self.partition(partition, weights)?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl<const D: usize> Algorithm<D> for coupe::VnFirst {
-    fn run(&mut self, partition: &mut [usize], problem: &Problem<D>) -> Result<()> {
-        use weight::Array::*;
-        match &problem.weights {
-            Integers(is) => {
-                let weights: Vec<_> = is.iter().map(|weight| weight[0]).collect();
-                self.partition(partition, &weights)?;
-            }
-            Floats(fs) => {
-                let weights: Vec<_> = fs.iter().map(|weight| weight[0]).collect();
-                self.partition(partition, &weights)?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl<const D: usize> Algorithm<D> for coupe::Rcb {
-    fn run(&mut self, partition: &mut [usize], problem: &Problem<D>) -> Result<()> {
-        use weight::Array::*;
-        let points = problem.points.par_iter().cloned();
-        match &problem.weights {
-            Integers(is) => {
-                let weights = is.par_iter().map(|weight| weight[0]);
-                self.partition(partition, (points, weights))?;
-            }
-            Floats(fs) => {
-                let weights = fs.par_iter().map(|weight| weight[0]);
-                self.partition(partition, (points, weights))?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl<const D: usize> Algorithm<D> for coupe::HilbertCurve {
-    fn run(&mut self, partition: &mut [usize], problem: &Problem<D>) -> Result<()> {
-        use weight::Array::*;
-        if D != 2 {
-            anyhow::bail!("hilbert is only implemented for 2D meshes");
-        }
-        // SAFETY: is a noop since D == 2
-        let points =
-            unsafe { mem::transmute::<&Vec<PointND<D>>, &Vec<PointND<2>>>(&problem.points) };
-        match &problem.weights {
-            Integers(_) => anyhow::bail!("hilbert is only implemented for floats"),
-            Floats(fs) => {
-                let weights: Vec<f64> = fs.iter().map(|weight| weight[0]).collect();
-                self.partition(partition, (points, weights))?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl<const D: usize> Algorithm<D> for coupe::FiducciaMattheyses {
-    fn run(&mut self, partition: &mut [usize], problem: &Problem<D>) -> Result<()> {
-        use weight::Array::*;
-        let adjacency = problem.adjacency.view();
-        match &problem.weights {
-            Integers(is) => {
-                let weights: Vec<i64> = is.iter().map(|weight| weight[0]).collect();
-                self.partition(partition, (adjacency, &weights))?;
-            }
-            Floats(fs) => {
-                let weights: Vec<f64> = fs.iter().map(|weight| weight[0]).collect();
-                self.partition(partition, (adjacency, &weights))?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl<const D: usize> Algorithm<D> for coupe::KernighanLin {
-    fn run(&mut self, partition: &mut [usize], problem: &Problem<D>) -> Result<()> {
-        use weight::Array::*;
-        let adjacency = problem.adjacency.view();
-        match &problem.weights {
-            Integers(_) => anyhow::bail!("kl is only implemented for floats"),
-            Floats(fs) => {
-                let weights: Vec<f64> = fs.iter().map(|weight| weight[0]).collect();
-                self.partition(partition, (adjacency, &weights))?;
-            }
-        }
-        Ok(())
-    }
-}
-
-fn parse_algorithm<const D: usize>(spec: &str) -> Result<Box<dyn Algorithm<D>>> {
-    let mut args = spec.split(',');
-    let name = args.next().context("it's empty")?;
-
-    fn optional<T>(maybe_arg: Option<Result<T>>, default: T) -> Result<T> {
-        Ok(maybe_arg.transpose()?.unwrap_or(default))
-    }
-
-    fn require<T>(maybe_arg: Option<Result<T>>) -> Result<T> {
-        maybe_arg.context("not enough arguments")?
-    }
-
-    fn parse<T>(arg: Option<&str>) -> Option<Result<T>>
-    where
-        T: std::str::FromStr + any::Any,
-        T::Err: std::error::Error + Send + Sync + 'static,
-    {
-        arg.map(|arg| {
-            let f = arg.parse::<T>().with_context(|| {
-                format!("arg {:?} is not a valid {}", arg, any::type_name::<T>())
-            })?;
-            Ok(f)
-        })
-    }
-
-    Ok(match name {
-        "random" => {
-            use rand::SeedableRng as _;
-
-            let part_count = require(parse(args.next()))?;
-            let seed: [u8; 32] = {
-                let mut bytes = args.next().unwrap_or("").as_bytes().to_vec();
-                bytes.resize(32_usize, 0_u8);
-                bytes.try_into().unwrap()
-            };
-            let rng = rand_pcg::Pcg64::from_seed(seed);
-            Box::new(coupe::Random { rng, part_count })
-        }
-        "greedy" => Box::new(coupe::Greedy {
-            part_count: require(parse(args.next()))?,
-        }),
-        "kk" => Box::new(coupe::KarmarkarKarp {
-            part_count: require(parse(args.next()))?,
-        }),
-        "ckk" => Box::new(coupe::CompleteKarmarkarKarp {
-            tolerance: require(parse(args.next()))?,
-        }),
-        "vn-best" => Box::new(coupe::VnBest {
-            part_count: require(parse(args.next()))?,
-        }),
-        "vn-first" => Box::new(coupe::VnFirst {
-            part_count: require(parse(args.next()))?,
-        }),
-        "rcb" => Box::new(coupe::Rcb {
-            iter_count: require(parse(args.next()))?,
-        }),
-        "hilbert" => Box::new(coupe::HilbertCurve {
-            part_count: require(parse(args.next()))?,
-            order: optional(parse(args.next()), 12)?,
-        }),
-        "fm" => {
-            let max_imbalance = parse(args.next()).transpose()?;
-            let max_bad_move_in_a_row = optional(parse(args.next()), 0)?;
-            let mut max_passes = parse(args.next()).transpose()?;
-            if max_passes == Some(0) {
-                max_passes = None;
-            }
-            let mut max_flips_per_pass = parse(args.next()).transpose()?;
-            if max_flips_per_pass == Some(0) {
-                max_flips_per_pass = None;
-            }
-            Box::new(coupe::FiducciaMattheyses {
-                max_imbalance,
-                max_bad_move_in_a_row,
-                max_passes,
-                max_flips_per_pass,
-            })
-        }
-        "kl" => Box::new(coupe::KernighanLin {
-            max_bad_move_in_a_row: optional(parse(args.next()), 1)?,
-            ..Default::default()
-        }),
-
-        #[cfg(feature = "metis")]
-        "metis:recursive" => Box::new(metis::Recursive {
-            part_count: require(parse(args.next()))?,
-        }),
-
-        #[cfg(feature = "metis")]
-        "metis:kway" => Box::new(metis::KWay {
-            part_count: require(parse(args.next()))?,
-        }),
-
-        #[cfg(feature = "scotch")]
-        "scotch:std" => Box::new(scotch::Standard {
-            part_count: require(parse(args.next()))?,
-        }),
-
-        _ => anyhow::bail!("unknown algorithm {:?}", name),
-    })
-}
-
 fn main_d<const D: usize>(
     matches: getopts::Matches,
     mesh: Mesh,
     weights: weight::Array,
 ) -> Result<Vec<usize>> {
-    let points: Vec<_> = mesh
-        .elements()
-        .filter_map(|(element_type, nodes, _element_ref)| {
-            if element_type.dimension() != mesh.dimension() {
-                return None;
-            }
-            let mut barycentre = [0.0; D];
-            for node_idx in nodes {
-                let node_coordinates = mesh.node(*node_idx);
-                for (bc_coord, node_coord) in barycentre.iter_mut().zip(node_coordinates) {
-                    *bc_coord += node_coord;
-                }
-            }
-            for bc_coord in &mut barycentre {
-                *bc_coord /= nodes.len() as f64;
-            }
-            Some(PointND::from(barycentre))
-        })
-        .collect();
-
-    let adjacency = coupe_tools::dual(&mesh);
-
-    let mut partition = vec![0; points.len()];
-    let problem = Problem {
-        points,
+    let problem = coupe_tools::Problem {
+        points: coupe_tools::barycentres::<D>(&mesh),
         weights,
-        adjacency,
+        adjacency: coupe_tools::dual(&mesh),
     };
+    let mut partition = vec![0; problem.points.len()];
 
     let algorithms: Vec<_> = matches
         .opt_strs("a")
         .into_iter()
         .map(|algorithm_spec| {
-            parse_algorithm(&algorithm_spec)
+            coupe_tools::parse_algorithm(&algorithm_spec)
                 .with_context(|| format!("invalid algorithm {:?}", algorithm_spec))
         })
         .collect::<Result<_>>()?;
 
     for mut algorithm in algorithms {
-        algorithm
-            .run(&mut partition, &problem)
-            .context("failed to apply algorithm")?;
+        let mut algorithm = algorithm.to_runner(&problem);
+        algorithm(&mut partition).context("failed to apply algorithm")?;
     }
 
     Ok(partition)
---
+++ tools/src/metis.rs
@@ -0,0 +1,70 @@
+use super::runner_error;
+use super::Problem;
+use super::Runner;
+use super::ToRunner;
+use mesh_io::weight;
+use metis::Idx;
+
+pub struct Recursive {
+    pub part_count: Idx,
+}
+
+impl<const D: usize> ToRunner<D> for Recursive {
+    fn to_runner<'a>(&'a mut self, problem: &'a Problem<D>) -> Runner<'a> {
+        let weights = match &problem.weights {
+            weight::Array::Integers(is) => is,
+            weight::Array::Floats(_) => {
+                return runner_error("MeTiS does not support float weights")
+            }
+        };
+        let ncon = weights.first().map_or(1, Vec::len) as Idx;
+        let mut weights: Vec<_> = weights.iter().flatten().map(|i| *i as Idx).collect();
+
+        let (xadj, adjncy, _) = problem.adjacency.view().into_raw_storage();
+        let mut xadj: Vec<_> = xadj.iter().map(|i| *i as Idx).collect();
+        let mut adjncy: Vec<_> = adjncy.iter().map(|i| *i as Idx).collect();
+
+        let mut metis_partition = vec![0; weights.len()];
+        Box::new(move |partition| {
+            metis::Graph::new(ncon, self.part_count, &mut xadj, &mut adjncy)
+                .set_vwgt(&mut weights)
+                .part_recursive(&mut metis_partition)?;
+            for (dst, src) in partition.iter_mut().zip(&mut metis_partition) {
+                *dst = *src as usize;
+            }
+            Ok(())
+        })
+    }
+}
+
+pub struct KWay {
+    pub part_count: Idx,
+}
+
+impl<const D: usize> ToRunner<D> for KWay {
+    fn to_runner<'a>(&'a mut self, problem: &'a Problem<D>) -> Runner<'a> {
+        let weights = match &problem.weights {
+            weight::Array::Integers(is) => is,
+            weight::Array::Floats(_) => {
+                return runner_error("MeTiS does not support float weights")
+            }
+        };
+        let ncon = weights.first().map_or(1, Vec::len) as Idx;
+        let mut weights: Vec<_> = weights.iter().flatten().map(|i| *i as Idx).collect();
+
+        let (xadj, adjncy, _) = problem.adjacency.view().into_raw_storage();
+        let mut xadj: Vec<_> = xadj.iter().map(|i| *i as Idx).collect();
+        let mut adjncy: Vec<_> = adjncy.iter().map(|i| *i as Idx).collect();
+
+        let mut metis_partition = vec![0; weights.len()];
+        Box::new(move |partition| {
+            metis::Graph::new(ncon, self.part_count, &mut xadj, &mut adjncy)
+                .set_vwgt(&mut weights)
+                .part_kway(&mut metis_partition)?;
+            for (dst, src) in partition.iter_mut().zip(&mut metis_partition) {
+                *dst = *src as usize;
+            }
+            Ok(())
+        })
+    }
+}
