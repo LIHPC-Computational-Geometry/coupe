386066905273a2fb086e86de1f568f8792007cc3
Hubert Hirtz <hubert@hirtz.pm>
2022-04-13 10:30:02 +0200

    fm: multi-thread trying some things... (partition AtomicUsize)

--- src/algorithms/fiduccia_mattheyses.rs
+++ src/algorithms/fiduccia_mattheyses.rs
@@ -1,6 +1,15 @@
 use super::Error;
+use rayon::iter::IndexedParallelIterator as _;
+use rayon::iter::IntoParallelIterator as _;
+use rayon::iter::IntoParallelRefIterator as _;
+use rayon::iter::ParallelIterator as _;
 use sprs::CsMatView;
 use std::collections::HashSet;
+use std::sync::atomic::AtomicI64;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+use std::sync::Mutex;
+use std::sync::RwLock;
 
 fn fiduccia_mattheyses<W>(
     partition: &mut [usize],
@@ -11,7 +20,7 @@ fn fiduccia_mattheyses<W>(
     max_imbalance: Option<f64>,
     max_bad_move_in_a_row: usize, // for each pass, the max number of subsequent moves that will decrease the gain
 ) where
-    W: std::fmt::Debug + Copy + PartialOrd,
+    W: std::fmt::Debug + Copy + Send + Sync + PartialOrd,
     W: std::iter::Sum + num::ToPrimitive,
     W: std::ops::AddAssign + std::ops::SubAssign + std::ops::Sub<Output = W> + num::Zero,
 {
@@ -49,67 +58,88 @@ fn fiduccia_mattheyses<W>(
         .max()
         .unwrap();
 
-    let gain_count = (2 * pmax + 1) as usize;
-    let mut gain_to_node = vec![HashSet::new(); gain_count].into_boxed_slice();
-    let mut node_to_gain = vec![None; adjacency.outer_dims()].into_boxed_slice();
+    let gain_to_node = (-pmax..=pmax)
+        .map(|_| Mutex::new(HashSet::new()))
+        .collect::<Vec<_>>()
+        .into_boxed_slice();
+    let node_to_gain = (0..adjacency.outer_dims())
+        .map(|_| AtomicI64::new(i64::MIN))
+        .collect::<Vec<_>>()
+        .into_boxed_slice();
     let gain_table_idx = |gain: i64| (gain + pmax) as usize;
 
     let mut best_cut_size = crate::topology::cut_size(adjacency, partition);
     tracing::info!("Initial cut size: {}", best_cut_size);
+
+    let partition = unsafe { std::mem::transmute::<&mut [usize], &[AtomicUsize]>(partition) };
 
     for _ in 0..max_passes {
         // monitors for each pass the number of subsequent flips
         // that increase cut size. It may be beneficial in some
         // situations to allow a certain amount of them. Performing bad flips can open
         // up new sequences of good flips.
-        let mut num_bad_move = 0;
+        let num_bad_move = AtomicUsize::new(0);
 
-        // Avoid copying partition arrays around and instead record an history
-        // of flips.
-        let mut flip_history = Vec::new();
-        let mut cut_size_history = Vec::new();
-
-        for set in &mut *gain_to_node {
-            set.clear();
+        for set in &*gain_to_node {
+            set.try_lock().unwrap().clear();
         }
-        for (node, initial_part) in partition.iter().enumerate() {
-            let target_part = 1 - *initial_part;
-            let gain = adjacency
-                .outer_view(node)
-                .unwrap()
-                .iter()
-                .map(|(neighbor, edge_weight)| {
-                    if partition[neighbor] == *initial_part {
-                        -*edge_weight
-                    } else if partition[neighbor] == target_part {
-                        *edge_weight
-                    } else {
-                        0
-                    }
-                })
-                .sum();
-            node_to_gain[node] = Some(gain);
-            gain_to_node[gain_table_idx(gain)].insert(node);
+        partition
+            .par_iter()
+            .enumerate()
+            .for_each(|(node, initial_part)| {
+                let initial_part = initial_part.load(Ordering::Relaxed);
+                let gain = adjacency
+                    .outer_view(node)
+                    .unwrap()
+                    .iter()
+                    .map(|(neighbor, edge_weight)| {
+                        let neighbor_part = partition[neighbor].load(Ordering::Relaxed);
+                        if neighbor_part == initial_part {
+                            -*edge_weight
+                        } else {
+                            // neighbor_part == 1 - initial_part
+                            *edge_weight
+                        }
+                    })
+                    .sum();
+                node_to_gain[node].store(gain, Ordering::Relaxed);
+                gain_to_node[gain_table_idx(gain)]
+                    .lock()
+                    .unwrap()
+                    .insert(node);
+            });
+
+        struct Synchronized<'a, W> {
+            part_weights: &'a mut [W],
+            /// Keep track of (node, initial_part) to rewind bad moves.
+            flip_history: Vec<(usize, usize)>,
+            /// Also keep track of cut sizes.
+            cut_size_history: Vec<i64>,
         }
 
-        // enter pass loop
-        // The number of iteration of the pas loop is at most the
-        // number of nodes in the mesh. However, if too many subsequent
-        // bad flips are performed, the loop will break early
-        for _ in 0..max_flips_per_pass {
-            let (max_pos, max_gain) = match gain_to_node
+        let s = RwLock::new(Synchronized {
+            part_weights: &mut parts_weights,
+            flip_history: Vec::new(),
+            cut_size_history: Vec::new(),
+        });
+
+        (0..max_flips_per_pass).into_par_iter().try_for_each(|_| {
+            let part_weights_copy = s.read().unwrap().part_weights.to_vec();
+            let (max_pos, max_gain) = gain_to_node
                 .iter()
                 .rev()
                 .zip((-pmax..=pmax).rev())
                 .find_map(|(nodes, gain)| {
                     let (best_node, _) = nodes
+                        .try_lock()
+                        .ok()?
                         .iter()
                         .filter_map(|node| {
                             let weight = weights[*node];
-                            let initial_part = partition[*node];
+                            let initial_part = partition[*node].load(Ordering::Relaxed);
                             let target_part = 1 - initial_part;
-                            let initial_part_weight = parts_weights[initial_part] - weight;
-                            let target_part_weight = parts_weights[target_part] + weight;
+                            let initial_part_weight = part_weights_copy[initial_part] - weight;
+                            let target_part_weight = part_weights_copy[target_part] + weight;
                             let imbalance = [initial_part_weight, target_part_weight]
                                 .iter()
                                 .map(part_imbalance)
@@ -126,80 +156,92 @@ fn fiduccia_mattheyses<W>(
                             f64::partial_cmp(imbalance0, imbalance1).unwrap()
                         })?;
                     Some((best_node, gain))
-                }) {
-                Some(v) => v,
-                None => break,
-            };
+                })?;
 
             if max_gain <= 0 {
+                let num_bad_move = num_bad_move.fetch_add(1, Ordering::Relaxed);
                 if num_bad_move >= max_bad_move_in_a_row {
                     tracing::info!("reached max bad move in a row");
-                    break;
+                    return None;
                 }
-                num_bad_move += 1;
             } else {
                 // a good move breaks the bad moves sequence
-                num_bad_move = 0;
+                num_bad_move.store(0, Ordering::Relaxed);
             }
 
-            node_to_gain[max_pos] = None;
-            gain_to_node[gain_table_idx(max_gain)].remove(&max_pos);
-            let target_part = 1 - partition[max_pos];
+            let was_present = gain_to_node[gain_table_idx(max_gain)]
+                .lock()
+                .unwrap()
+                .remove(&max_pos);
+            if !was_present {
+                // The node has been already moved by another thread.
+                return Some(());
+            }
+            node_to_gain[max_pos].store(i64::MIN, Ordering::Relaxed);
 
-            // flip node
-            let old_part = std::mem::replace(&mut partition[max_pos], target_part);
-            flip_history.push((max_pos, old_part, target_part));
-            parts_weights[old_part] -= weights[max_pos];
-            parts_weights[target_part] += weights[max_pos];
+            let old_part = partition[max_pos].load(Ordering::Relaxed);
+            let target_part = 1 - old_part;
+            partition[max_pos].store(target_part, Ordering::Relaxed);
 
-            // save cut_size
-            tracing::info!(max_pos, old_part, target_part, "flip");
-            let mut new_cut_size = *cut_size_history.last().unwrap_or(&best_cut_size);
-            for (neighbors, edge_weight) in adjacency.outer_view(max_pos).unwrap().iter() {
-                if partition[neighbors] == old_part {
-                    new_cut_size += edge_weight;
-                } else if partition[neighbors] == target_part {
-                    new_cut_size -= edge_weight;
+            {
+                let mut s = s.write().unwrap();
+                s.flip_history.push((max_pos, old_part));
+                s.part_weights[old_part] -= weights[max_pos];
+                s.part_weights[target_part] += weights[max_pos];
+
+                // save cut_size
+                tracing::info!(max_pos, old_part, target_part, "flip");
+                let mut new_cut_size = *s.cut_size_history.last().unwrap_or(&best_cut_size);
+                for (neighbors, edge_weight) in adjacency.outer_view(max_pos).unwrap().iter() {
+                    if partition[neighbors].load(Ordering::Relaxed) == old_part {
+                        new_cut_size += edge_weight;
+                    } else {
+                        new_cut_size -= edge_weight;
+                    }
                 }
+                s.cut_size_history.push(new_cut_size);
             }
-            debug_assert_eq!(
-                new_cut_size,
-                crate::topology::cut_size(adjacency, partition),
-            );
-            cut_size_history.push(new_cut_size);
 
             for (neighbor, _) in adjacency.outer_view(max_pos).unwrap().iter() {
-                let initial_part = partition[neighbor];
-                let target_part = 1 - initial_part;
-                let outdated_gain = match node_to_gain[neighbor] {
-                    Some(v) => v,
-                    None => continue,
-                };
+                let initial_part = partition[neighbor].load(Ordering::Relaxed);
+                let outdated_gain = node_to_gain[neighbor].load(Ordering::Relaxed);
+                if outdated_gain == i64::MIN {
+                    continue;
+                }
                 let updated_gain = adjacency
                     .outer_view(neighbor)
                     .unwrap()
                     .iter()
                     .map(|(neighbor, edge_weight)| {
-                        if partition[neighbor] == initial_part {
+                        if partition[neighbor].load(Ordering::Relaxed) == initial_part {
                             -*edge_weight
-                        } else if partition[neighbor] == target_part {
-                            *edge_weight
                         } else {
-                            0
+                            *edge_weight
                         }
                     })
                     .sum();
                 if outdated_gain != updated_gain {
-                    gain_to_node[gain_table_idx(outdated_gain)].remove(&neighbor);
-                    gain_to_node[gain_table_idx(updated_gain)].insert(neighbor);
+                    gain_to_node[gain_table_idx(outdated_gain)]
+                        .lock()
+                        .unwrap()
+                        .remove(&neighbor);
+                    gain_to_node[gain_table_idx(updated_gain)]
+                        .lock()
+                        .unwrap()
+                        .insert(neighbor);
                 }
             }
-        }
+
+            Some(())
+        });
 
         let old_cut_size = best_cut_size;
 
+        let mut s = s.into_inner().unwrap();
+
         // lookup for best cutsize
-        let (best_pos, best_cut) = match cut_size_history
+        let (best_pos, best_cut) = match s
+            .cut_size_history
             .iter()
             .cloned()
             .enumerate()
@@ -212,12 +254,12 @@ fn fiduccia_mattheyses<W>(
         tracing::info!(
             "rewinding flips from pos {} to pos {}",
             best_pos + 1,
-            flip_history.len()
+            s.flip_history.len()
         );
-        for (idx, old_part, target_part) in flip_history.drain(best_pos + 1..) {
-            partition[idx] = old_part;
+        for (idx, old_part) in s.flip_history.drain(best_pos + 1..) {
+            partition[idx].store(old_part, Ordering::Relaxed);
             parts_weights[old_part] += weights[idx];
-            parts_weights[target_part] += weights[idx];
+            parts_weights[1 - old_part] += weights[idx];
         }
 
         best_cut_size = best_cut;
@@ -318,7 +360,7 @@ pub struct FiducciaMattheyses {
 
 impl<'a, W> crate::Partition<(CsMatView<'a, i64>, &'a [W])> for FiducciaMattheyses
 where
-    W: std::fmt::Debug + Copy + PartialOrd + num::Zero,
+    W: std::fmt::Debug + Copy + Send + Sync + PartialOrd + num::Zero,
     W: std::iter::Sum + num::ToPrimitive,
     W: std::ops::AddAssign + std::ops::SubAssign + std::ops::Sub<Output = W>,
 {
