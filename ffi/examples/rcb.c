f4b837bdb9cb758df61d1b6afdc64f66e7b7ce49
Hubert Hirtz <hubert@hirtz.pm>
2022-04-26 15:30:32 +0200

    Add a C FFI to coupe
    So that coupe can be used from most languages, not just rust.
    
    The bindings uses `uintptr_t` extensively, since it's the equivalent of
    `usize` (and `size_t` is not[0]).
    
    The C interface follows this pattern:
    
    A `coupe_data` opaque type can be constructed in several ways, to allow
    the user to feed coupe elements in different ways.
    
    For example, if the user has an array of weights, they can use:
    
        double weights[4] = {1.2, 3.4, 5.6, 7.8};
        coupe_data *w =
            coupe_data_array(4, COUPE_DOUBLE, weights);
    
    or if they don't want to use weights:
    
        int one = 1;
        coupe_data *w = coupe_data_constant(weight_count, COUPE_INT, &one);
    
    (very idiomatic C!  unfortunately for these function to be "generic" the
    second argument has to be a `void *`)
    
    and if they have a complex data structure that can be indexed:
    
        ComplexWeightCollection *weights = /* ... */
        void *coll_get_ith(ComplexWeightCollection *coll, uintptr_t i) {
            /* ... */
        }
        coupe_data *w = coupe_data_fn(weights, weight_count, coll_get_ith);
    
    For topologic algorithms, a `coupe_adjncy` type is defined.  For now it
    can only be constructed from compressed adjacency matrices.
    
    Algorithms are implemented as functions that take:
    
    - the partition array,
    - some `coupe_data` for the points and weights,
    - some `coupe_adjncy` for the topology,
    - the algorithms parameters.
    
    They return an error if any.  Maybe some error can be returned but the
    partition still be populated and valid.
    
    [0] https://rust-lang.github.io/unsafe-code-guidelines/layout/scalars.html#isize-and-usize

---
+++ ffi/examples/rcb.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "../include/coupe.h"
+
+#define POINT_COUNT 4
+#define DIMENSION 2
+
+int
+main()
+{
+	uintptr_t partition[POINT_COUNT];
+
+	double point_array[POINT_COUNT][DIMENSION] = {
+		{0.0, 0.0},
+		{0.0, 1.0},
+		{1.0, 0.0},
+		{1.0, 1.0},
+	};
+	struct coupe_data *points = coupe_data_array(POINT_COUNT, COUPE_DOUBLE, point_array);
+
+	int one = 1;
+	struct coupe_data *weights = coupe_data_constant(POINT_COUNT, COUPE_INT, &one);
+
+	uintptr_t iter_count = 1;
+	double tolerance = 0.05;
+	enum coupe_err err =
+		coupe_rcb(partition, DIMENSION, points, weights, iter_count, tolerance);
+
+	printf("With 1 iteration (2 parts), RCB returned: %s\n", coupe_strerror(err));
+	printf("partition:\n");
+	printf("%ld %ld\n%ld %ld\n", partition[0], partition[1], partition[2], partition[3]);
+
+	iter_count = 2;
+	err = coupe_rcb(partition, DIMENSION, points, weights, iter_count, tolerance);
+
+	printf("With 2 iterations (4 parts), RCB returned: %s\n", coupe_strerror(err));
+	printf("partition:\n");
+	printf("%ld %ld\n%ld %ld\n", partition[0], partition[1], partition[2], partition[3]);
+
+	coupe_data_free(points);
+	coupe_data_free(weights);
+	return 0;
+}
---
+++ ffi/src/lib.rs
@@ -0,0 +1,510 @@
+#![allow(clippy::missing_safety_doc)] // See include/coupe.h
+
+use crate::data::Data;
+use crate::data::Type;
+use coupe::Partition as _;
+use coupe::Point2D;
+use coupe::PointND;
+use coupe::Real;
+use nalgebra::allocator::Allocator;
+use nalgebra::ArrayStorage;
+use nalgebra::Const;
+use nalgebra::DefaultAllocator;
+use nalgebra::DimDiff;
+use nalgebra::DimSub;
+use nalgebra::ToTypenum;
+use std::ffi::c_void;
+use std::mem;
+use std::os::raw::c_char;
+use std::os::raw::c_int;
+use std::ptr;
+use std::slice;
+
+#[macro_use]
+mod data;
+
+fn box_try_new<T>(value: T) -> Option<Box<T>>
+where
+    T: std::panic::UnwindSafe,
+{
+    // TODO replace with [Box::try_new] once stabilised.
+    std::panic::catch_unwind(|| Some(Box::new(value))).unwrap_or(None)
+}
+
+#[repr(C)]
+pub enum Error {
+    Ok,
+    Alloc,
+    Crash,
+    BadDimension,
+    BadType,
+    BipartOnly,
+    LenMismatch,
+    NotFound,
+    NegValues,
+}
+
+impl From<coupe::Error> for Error {
+    fn from(err: coupe::Error) -> Self {
+        match err {
+            coupe::Error::NotFound => Self::NotFound,
+            coupe::Error::NegativeValues => Self::NegValues,
+            coupe::Error::BiPartitioningOnly => Self::BipartOnly,
+            coupe::Error::InputLenMismatch { .. } => Self::LenMismatch,
+            _ => unreachable!(),
+        }
+    }
+}
+
+/// Wrapper around [std::panic::catch_unwind] that returns [Error::Crash] on
+/// panic.
+fn catch_unwind<F>(f: F) -> Error
+where
+    F: FnOnce() -> Error + std::panic::UnwindSafe,
+{
+    std::panic::catch_unwind(f).unwrap_or(Error::Crash)
+}
+
+#[no_mangle]
+pub extern "C" fn coupe_strerror(err: Error) -> *const c_char {
+    match err {
+        Error::Ok => {
+            static MSG: &[u8] = b"success\0";
+            MSG.as_ptr() as *const c_char
+        }
+        Error::Alloc => {
+            static MSG: &[u8] = b"allocation failed\0";
+            MSG.as_ptr() as *const c_char
+        }
+        Error::Crash => {
+            static MSG: &[u8] = b"coupe encountered a bug and crashed\0";
+            MSG.as_ptr() as *const c_char
+        }
+        Error::BadDimension => {
+            static MSG: &[u8] = b"this algorithm does not support the given mesh dimension";
+            MSG.as_ptr() as *const c_char
+        }
+        Error::BadType => {
+            static MSG: &[u8] = b"this algorithm does not support the given type";
+            MSG.as_ptr() as *const c_char
+        }
+        Error::BipartOnly => {
+            static MSG: &[u8] = b"this algorithm does not support k-way partitioning\0";
+            MSG.as_ptr() as *const c_char
+        }
+        Error::LenMismatch => {
+            static MSG: &[u8] = b"input iters (e.g. weights and points) don't have the same length";
+            MSG.as_ptr() as *const c_char
+        }
+        Error::NotFound => {
+            static MSG: &[u8] = b"no partition has been found for the given constraints\0";
+            MSG.as_ptr() as *const c_char
+        }
+        Error::NegValues => {
+            static MSG: &[u8] = b"this algorithm does not support negative values\0";
+            MSG.as_ptr() as *const c_char
+        }
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_data_free(data: *mut Data) {
+    if !data.is_null() {
+        let data = Box::from_raw(data);
+        mem::drop(data);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn coupe_data_array(len: usize, type_: Type, array: *const c_void) -> *mut Data {
+    let data = Data::Array(data::Array { len, type_, array });
+    match box_try_new(data) {
+        Some(v) => Box::into_raw(v),
+        None => ptr::null_mut(),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn coupe_data_constant(len: usize, type_: Type, value: *const c_void) -> *mut Data {
+    let data = Data::Constant(data::Constant { len, type_, value });
+    match box_try_new(data) {
+        Some(v) => Box::into_raw(v),
+        None => ptr::null_mut(),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn coupe_data_fn(
+    context: *const c_void,
+    len: usize,
+    type_: Type,
+    i_th: extern "C" fn(*const c_void, usize) -> *const c_void,
+) -> *mut Data {
+    let data = Data::Fn(data::Fn {
+        len,
+        type_,
+        context,
+        i_th,
+    });
+    match box_try_new(data) {
+        Some(v) => Box::into_raw(v),
+        None => ptr::null_mut(),
+    }
+}
+
+pub enum Adjncy<'a> {
+    Int(sprs::CsMatView<'a, c_int>),
+    Int64(sprs::CsMatView<'a, i64>),
+    Double(sprs::CsMatView<'a, f64>),
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_adjncy_free(adjncy: *mut Adjncy) {
+    if !adjncy.is_null() {
+        let adjncy = Box::from_raw(adjncy);
+        mem::drop(adjncy);
+    }
+}
+
+unsafe fn adjncy_csr_unchecked(
+    size: usize,
+    xadj: *const usize,
+    adjncy: *const usize,
+    data_type: Type,
+    data: *const c_void,
+) -> Adjncy<'static> {
+    let xadj = slice::from_raw_parts(xadj, size + 1);
+    let adjncy = slice::from_raw_parts(adjncy, xadj[xadj.len() - 1]);
+    match data_type {
+        Type::Int => {
+            let data = slice::from_raw_parts(data as *const c_int, adjncy.len());
+            let matrix =
+                sprs::CsMatView::new_unchecked(sprs::CSR, (size, size), xadj, adjncy, data);
+            Adjncy::Int(matrix)
+        }
+        Type::Int64 => {
+            let data = slice::from_raw_parts(data as *const i64, adjncy.len());
+            let matrix =
+                sprs::CsMatView::new_unchecked(sprs::CSR, (size, size), xadj, adjncy, data);
+            Adjncy::Int64(matrix)
+        }
+        Type::Double => {
+            let data = slice::from_raw_parts(data as *const f64, adjncy.len());
+            let matrix =
+                sprs::CsMatView::new_unchecked(sprs::CSR, (size, size), xadj, adjncy, data);
+            Adjncy::Double(matrix)
+        }
+    }
+}
+
+fn adjncy_ptr(adjacency: Adjncy<'_>) -> *mut Adjncy<'_> {
+    match box_try_new(adjacency) {
+        Some(v) => Box::into_raw(v),
+        None => ptr::null_mut(),
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_adjncy_csr(
+    size: usize,
+    xadj: *const usize,
+    adjncy: *const usize,
+    data_type: Type,
+    data: *const c_void,
+) -> *mut Adjncy<'static> {
+    let adjacency = adjncy_csr_unchecked(size, xadj, adjncy, data_type, data);
+    match adjacency {
+        Adjncy::Int(matrix) => {
+            if matrix.check_compressed_structure().is_err() {
+                return ptr::null_mut();
+            }
+        }
+        Adjncy::Int64(matrix) => {
+            if matrix.check_compressed_structure().is_err() {
+                return ptr::null_mut();
+            }
+        }
+        Adjncy::Double(matrix) => {
+            if matrix.check_compressed_structure().is_err() {
+                return ptr::null_mut();
+            }
+        }
+    }
+    adjncy_ptr(adjacency)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_adjncy_csr_unchecked(
+    size: usize,
+    xadj: *const usize,
+    adjncy: *const usize,
+    data_type: Type,
+    data: *const c_void,
+) -> *mut Adjncy<'static> {
+    let adjacency = adjncy_csr_unchecked(size, xadj, adjncy, data_type, data);
+    adjncy_ptr(adjacency)
+}
+
+unsafe fn coupe_rcb_d<const D: usize>(
+    partition: &mut [usize],
+    points: &Data,
+    weights: &Data,
+    mut algo: coupe::Rcb,
+) -> Error {
+    let res = with_par_iter!(points, PointND<D>, {
+        with_par_iter!(weights, { algo.partition(partition, (points, weights)) })
+    });
+    match res {
+        Ok(_) => Error::Ok,
+        Err(err) => Error::from(err),
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_rcb(
+    partition: *mut usize,
+    dimension: usize,
+    points: *const Data,
+    weights: *const Data,
+    iter_count: usize,
+    tolerance: f64,
+) -> Error {
+    let points = &*points;
+    let weights = &*weights;
+
+    let element_count = points.len();
+    if element_count != weights.len() {
+        return Error::LenMismatch;
+    }
+
+    let algo = coupe::Rcb {
+        iter_count,
+        tolerance,
+        ..Default::default()
+    };
+
+    catch_unwind(|| {
+        let partition = slice::from_raw_parts_mut(partition, element_count);
+        match dimension {
+            2 => coupe_rcb_d::<2>(partition, points, weights, algo),
+            3 => coupe_rcb_d::<3>(partition, points, weights, algo),
+            _ => Error::BadDimension,
+        }
+    })
+}
+
+unsafe fn coupe_rib_d<const D: usize>(
+    partition: &mut [usize],
+    points: &Data,
+    weights: &Data,
+    mut algo: coupe::Rib,
+) -> Error
+where
+    Const<D>: DimSub<Const<1>> + ToTypenum,
+    DefaultAllocator: Allocator<f64, Const<D>, Const<D>, Buffer = ArrayStorage<f64, D, D>>
+        + Allocator<f64, DimDiff<Const<D>, Const<1>>>,
+{
+    let points = match points.to_slice::<PointND<D>>() {
+        Ok(v) => v,
+        Err(_) => return Error::Alloc,
+    };
+    let res = with_par_iter!(weights, {
+        algo.partition(partition, (points.as_ref(), weights))
+    });
+    match res {
+        Ok(_) => Error::Ok,
+        Err(err) => Error::from(err),
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_rib(
+    partition: *mut usize,
+    dimension: usize,
+    points: *const Data,
+    weights: *const Data,
+    iter_count: usize,
+    tolerance: f64,
+) -> Error {
+    let points = &*points;
+    let weights = &*weights;
+
+    let element_count = points.len();
+    if element_count != weights.len() {
+        return Error::LenMismatch;
+    }
+
+    let algo = coupe::Rib {
+        iter_count,
+        tolerance,
+    };
+
+    catch_unwind(|| {
+        let partition = slice::from_raw_parts_mut(partition, element_count);
+        match dimension {
+            2 => coupe_rib_d::<2>(partition, points, weights, algo),
+            3 => coupe_rib_d::<3>(partition, points, weights, algo),
+            _ => Error::BadDimension,
+        }
+    })
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_hilbert(
+    partition: *mut usize,
+    points: *const Data,
+    weights: *const Data,
+    part_count: usize,
+    order: u32,
+) -> Error {
+    let points = &*points;
+    let weights = &*weights;
+
+    let element_count = points.len();
+    if element_count != weights.len() {
+        return Error::LenMismatch;
+    }
+    if weights.type_() != Type::Double {
+        return Error::BadType;
+    }
+
+    catch_unwind(|| {
+        let partition = slice::from_raw_parts_mut(partition, element_count);
+
+        let points = match points.to_slice::<Point2D>() {
+            Ok(v) => v,
+            Err(_) => return Error::Alloc,
+        };
+        let weights = match weights.to_slice::<f64>() {
+            Ok(v) => v,
+            Err(_) => return Error::Alloc,
+        };
+
+        let res = coupe::HilbertCurve { part_count, order }.partition(partition, (points, weights));
+
+        match res {
+            Ok(()) => Error::Ok,
+            Err(_) => Error::NotFound, // TODO use a proper error code
+        }
+    })
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_greedy(
+    partition: *mut usize,
+    weights: *const Data,
+    part_count: usize,
+) -> Error {
+    let weights = &*weights;
+    let element_count = weights.len();
+    catch_unwind(|| {
+        let partition = slice::from_raw_parts_mut(partition, element_count);
+        let mut algo = coupe::Greedy { part_count };
+        match with_iter!(weights, { algo.partition(partition, weights) }) {
+            Ok(()) => Error::Ok,
+            Err(err) => Error::from(err),
+        }
+    })
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_karmarkar_karp(
+    partition: *mut usize,
+    weights: *const Data,
+    part_count: usize,
+) -> Error {
+    let weights = &*weights;
+    let element_count = weights.len();
+
+    catch_unwind(|| {
+        let partition = slice::from_raw_parts_mut(partition, element_count);
+
+        let mut algo = coupe::KarmarkarKarp { part_count };
+
+        let res = match weights.type_() {
+            Type::Int => {
+                with_iter!(weights, c_int, { algo.partition(partition, weights) })
+            }
+            Type::Int64 => {
+                with_iter!(weights, i64, { algo.partition(partition, weights) })
+            }
+            Type::Double => {
+                with_iter!(weights, Real, { algo.partition(partition, weights) })
+            }
+        };
+
+        match res {
+            Ok(()) => Error::Ok,
+            Err(err) => Error::from(err),
+        }
+    })
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_karmarkar_karp_complete(
+    partition: *mut usize,
+    weights: *const Data,
+    tolerance: f64,
+) -> Error {
+    let weights = &*weights;
+    let element_count = weights.len();
+    catch_unwind(|| {
+        let partition = slice::from_raw_parts_mut(partition, element_count);
+        let mut algo = coupe::CompleteKarmarkarKarp { tolerance };
+        match with_iter!(weights, { algo.partition(partition, weights) }) {
+            Ok(()) => Error::Ok,
+            Err(err) => Error::from(err),
+        }
+    })
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn coupe_fiduccia_mattheyses(
+    partition: *mut usize,
+    adjncy: *const Adjncy<'_>,
+    weights: *const Data,
+    max_passes: usize,
+    max_moves_per_pass: usize,
+    max_imbalance: f64,
+    max_bad_moves_in_a_row: usize,
+) -> Error {
+    let weights = &*weights;
+    let element_count = weights.len();
+
+    let mut algo = coupe::FiducciaMattheyses {
+        max_passes: if max_passes == 0 {
+            None
+        } else {
+            Some(max_passes)
+        },
+        max_moves_per_pass: if max_moves_per_pass == 0 {
+            None
+        } else {
+            Some(max_moves_per_pass)
+        },
+        max_imbalance: if max_imbalance <= 0.0 {
+            None
+        } else {
+            Some(max_imbalance)
+        },
+        max_bad_move_in_a_row: max_bad_moves_in_a_row,
+    };
+
+    let adjacency = match &*adjncy {
+        Adjncy::Int64(matrix) => *matrix,
+        _ => return Error::BadType,
+    };
+
+    catch_unwind(move || {
+        let partition = slice::from_raw_parts_mut(partition, element_count);
+
+        let res = with_slice!(weights, {
+            algo.partition(partition, (adjacency, weights.as_ref()))
+        });
+
+        match res {
+            Ok(_) => Error::Ok, // TODO use metadata
+            Err(err) => Error::from(err),
+        }
+    })
+}
