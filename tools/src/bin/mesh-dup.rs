9c5b07480c6fcf1aa761ac5170e335553a483b81
Hubert Hirtz <hubert@hirtz.pm>
2022-06-22 13:49:38 +0200

    tools: add a mesh duplication tool
    Used to duplicate a mesh N times for finer-grained weak scaling
    measurements.
    
    In a future commit, it should be possible to connect the duplicates
    together by mirroring half of them.

---
+++ tools/src/bin/mesh-dup.rs
@@ -0,0 +1,42 @@
+use anyhow::Context as _;
+use anyhow::Result;
+use mesh_io::medit::Mesh;
+use std::env;
+use std::io;
+
+const USAGE: &str = "Usage: mesh-dup [options] <in.mesh >out.mesh";
+
+fn main() -> Result<()> {
+    let mut options = getopts::Options::new();
+    options.optflag("h", "help", "print this help menu");
+    options.optopt("f", "format", "output format", "EXT");
+    options.optopt("n", "times", "numbers of duplicates (default: 2)", "NUMBER");
+
+    let matches = options.parse(env::args().skip(1))?;
+
+    if matches.opt_present("h") {
+        eprintln!("{}", options.usage(USAGE));
+        return Ok(());
+    }
+    if !matches.free.is_empty() {
+        anyhow::bail!("too many arguments\n\n{}", options.usage(USAGE));
+    }
+
+    let format: coupe_tools::MeshFormat = matches
+        .opt_get("f")
+        .context("invalid value for option 'format'")?
+        .unwrap_or(coupe_tools::MeshFormat::MeditBinary);
+
+    let n: usize = matches
+        .opt_get("n")
+        .context("invalid value for option 'times'")?
+        .unwrap_or(2);
+
+    let stdin = io::stdin();
+    let stdin = stdin.lock();
+    let stdin = io::BufReader::new(stdin);
+    let mesh = Mesh::from_reader(stdin).context("failed to read mesh")?;
+    coupe_tools::write_mesh(&mesh.duplicate(n), format)?;
+
+    Ok(())
+}
