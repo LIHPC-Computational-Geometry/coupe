568146030018ab64111fd283ff2cbeeefe073199
Hubert Hirtz <hubert@hirtz.pm>
2022-09-01 15:19:28 +0200

    report: add vnbest and vnfirst runs

--- tools/report/common.sh
+++ tools/report/common.sh
@@ -7,29 +7,57 @@ ALGORITHMS="
 -a rcb,1 -a arcswap,0.05
 -a rcb,1 -a fm,0.05
 -a rcb,1 -a fm,0.05,16
+-a rcb,1 -a vn-best
+-a rcb,1 -a vn-best -a arcswap,0.05
+-a rcb,1 -a vn-first
+-a rcb,1 -a vn-first -a arcswap,0.05
 -a hilbert,2
 -a hilbert,2 -a arcswap,0.05
 -a hilbert,2 -a fm,0.05
 -a hilbert,2 -a fm,0.05,16
+-a hilbert,2 -a vn-best
+-a hilbert,2 -a vn-best -a arcswap,0.05
+-a hilbert,2 -a vn-first
+-a hilbert,2 -a vn-first -a arcswap,0.05
 
 -a kk,3
 -a kk,3 -a arcswap,0.05
 -a hilbert,3
 -a hilbert,3 -a arcswap,0.05
+-a hilbert,3 -a vn-best
+-a hilbert,3 -a vn-best -a arcswap,0.05
+-a hilbert,3 -a vn-first
+-a hilbert,3 -a vn-first -a arcswap,0.05
 
 -a kk,4
 -a kk,4 -a arcswap,0.05
 -a rcb,2
 -a rcb,2 -a arcswap,0.05
+-a rcb,2 -a vn-best
+-a rcb,2 -a vn-best -a arcswap,0.05
+-a rcb,2 -a vn-first
+-a rcb,2 -a vn-first -a arcswap,0.05
 -a hilbert,4
 -a hilbert,4 -a arcswap,0.05
+-a hilbert,4 -a vn-best
+-a hilbert,4 -a vn-best -a arcswap,0.05
+-a hilbert,4 -a vn-first
+-a hilbert,4 -a vn-first -a arcswap,0.05
 
 -a kk,128
 -a kk,128 -a arcswap,0.05
 -a rcb,7
 -a rcb,7 -a arcswap,0.05
+-a rcb,7 -a vn-best
+-a rcb,7 -a vn-best -a arcswap,0.05
+-a rcb,7 -a vn-first
+-a rcb,7 -a vn-first -a arcswap,0.05
 -a hilbert,128
 -a hilbert,128 -a arcswap,0.05
+-a hilbert,128 -a vn-best
+-a hilbert,128 -a vn-best -a arcswap,0.05
+-a hilbert,128 -a vn-first
+-a hilbert,128 -a vn-first -a arcswap,0.05
 "
 
 WEIGHT_DISTRIBUTIONS="
