c487b5342cce0389e73c3b7b9ab0f2a961fd891d
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2023-07-26 16:41:03 +0200

    [new-rcb-impl] Rewrote `BBox2D::from_points` method
    The new implementation does not treat point coordinates as a single value anymore. The points defining the bounding box can thus be virtual and just define a box that encloses all points in the given set

--- new-rcb-impl/src/bounding_box.rs
+++ new-rcb-impl/src/bounding_box.rs
@@ -4,34 +4,43 @@ use cust::DeviceCopy;
 use itertools::Itertools;
 use itertools::MinMaxResult::{MinMax, NoElements, OneElement};
 
-#[derive(Clone, Copy, DeviceCopy, PartialEq, PartialOrd)]
+#[derive(Clone, Copy, DeviceCopy, Debug, PartialEq, PartialOrd)]
 pub struct BoundingBox<const D: usize> {
     p_min: PointND<D>,
     p_max: PointND<D>,
 }
 
-pub type BBox2D = BoundingBox<2>;
-impl BBox2D {
-    pub fn from_points(points: &[Point2D]) -> Option<Self> {
-        match points.iter().minmax() {
-            NoElements => None,
-            OneElement(p) => Some(Self {
-                p_min: *p,
-                p_max: *p,
-            }),
-            MinMax(p_min, p_max) => Some(Self {
-                p_min: *p_min,
-                p_max: *p_max,
-            }),
-        }
-    }
-
-    pub fn _p_min(&self) -> &Point2D {
+impl<const D: usize> BoundingBox<D> {
+    pub fn p_min(&self) -> &PointND<D> {
         &self.p_min
     }
 
-    pub fn _p_max(&self) -> &Point2D {
+    pub fn p_max(&self) -> &PointND<D> {
         &self.p_max
+    }
+}
+
+pub type BBox2D = BoundingBox<2>;
+impl BBox2D {
+    pub fn from_points(points: &[Point2D]) -> Option<Self> {
+        let x_minmax = match points.iter().map(|p| p.x()).minmax() {
+            NoElements => None,
+            OneElement(x) => Some((x, x)),
+            MinMax(x_min, x_max) => Some((x_min, x_max)),
+        };
+        let y_minmax = match points.iter().map(|p| p.y()).minmax() {
+            NoElements => None,
+            OneElement(y) => Some((y, y)),
+            MinMax(y_min, y_max) => Some((y_min, y_max)),
+        };
+
+        let (x_min, x_max) = x_minmax.unwrap();
+        let (y_min, y_max) = y_minmax.unwrap();
+
+        Some(Self {
+            p_min: Point2D::new(x_min, y_min),
+            p_max: Point2D::new(x_max, y_max),
+        })
     }
 }
 
@@ -49,7 +58,7 @@ mod tests {
         ];
         let bb = BBox2D::from_points(&points).unwrap();
 
-        assert_eq!(bb._p_min(), &Point2D::new(-1, 2));
-        assert_eq!(bb._p_max(), &Point2D::new(3, 2));
+        assert_eq!(bb.p_min(), &Point2D::new(-1, 1));
+        assert_eq!(bb.p_max(), &Point2D::new(3, 3));
     }
 }
